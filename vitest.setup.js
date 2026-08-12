a699279558c98d96f8c659de36f9232f85f6663d
Farhana Abdul Alim <204352779+Farhana-aa@users.noreply.github.com>
2026-08-12 10:23:39 +0100

    set AUTH_REDIRECT_URI in vitest setup

--- vitest.setup.js
+++ vitest.setup.js
@@ -1,3 +1,4 @@
 process.env.AUTH_TENANT_ID = 'test-tenant-id'
 process.env.AUTH_CLIENT_ID = 'test-client-id'
 process.env.AUTH_CLIENT_SECRET = 'test-client-secret'
+process.env.AUTH_REDIRECT_URI = 'http://localhost:3000/auth/callback'
